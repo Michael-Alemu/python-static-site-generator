b71db7670c85f31d88ad9533ef3d5744a4d37b3b
kingKong <msalemu13@ole.augie.edu>
2020-08-02 04:49:28 -0600

    adding custom data Structure for to handle content - wave 2

--- ssg/content.py
+++ ssg/content.py
@@ -3,18 +3,18 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
-        _, fm, content = cls.__regex.split(string, depth = 2)
-        load(fm, Loader=FullLoader)
-        cls(metadata, content)
+        _, fm, content = cls.__regex.split(string,  2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
-        data = metadata
-        self.data = {'content': content}
+        self.data = metadata
+        self.data["content"] = content
 
 
     @property
@@ -23,34 +23,24 @@ class Content(Mapping):
 
     @property
     def type(self):
-        return self.data["type"] if 'type' in self.data else None
+        return self.data["type"] if "type" in self.data else None
 
-    @data.setter
-    def data(self, value):
-        self.data['type'] = value
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
 
-    def __getitem__(self, item):
-        return self.data[item]
+    def __getitem__(self, key):
+        return self.data[key]
 
     def __iter__(self):
-        return iter(self.data)
+        return self.data.__iter__()
 
     def __len__(self):
-       len(self.data.keys())
+        len(self.data)
 
     def __repr__(self):
-        data ={}
+        data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
         return str(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
